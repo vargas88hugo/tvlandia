8c333f76b800140220a1c7d0f79c6b4725871b05
Hugo Vargas <vargas88hugo@gmail.com>
2020-05-22 19:24:40 -0500

    [Update] Added wait loop for technician until find one idle

--- src/tickets/ticket-client/ticket-client.service.ts
+++ src/tickets/ticket-client/ticket-client.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { InjectRepository } from '@nestjs/typeorm';
 import { Ticket } from 'src/tickets/ticket.entity';
@@ -6,12 +6,17 @@ import { Client } from 'src/users/clients/client.entity';
 import { CreateTicketDto } from '../dto/create-ticket.dto';
 import { TicketStatus } from '../helpers/ticket-status.enum';
 import { TicketsRepository } from '../tickets.repository';
+import { TechniciansRepository } from 'src/users/technicians/technicians.repository';
+import { TechnicianStatus } from 'src/users/technicians/helpers/technician-status.enum';
+import { Technician } from 'src/users/technicians/technician.entity';
 
 @Injectable()
 export class TicketClientService {
   constructor(
     @InjectRepository(TicketsRepository)
     private ticketRepository,
+    @InjectRepository(TechniciansRepository)
+    private technicianRepository,
   ) {}
 
   async createTicket(
@@ -28,6 +33,8 @@ export class TicketClientService {
     ticket.serviceType = serviceType;
     ticket.status = TicketStatus.HOLD;
     ticket.client = client;
+    ticket.technician = await this.chooseTechnician();
+    ticket.status = TicketStatus.INPROCESS;
 
     await this.ticketRepository.saveTicket(ticket);
 
@@ -44,5 +51,34 @@ export class TicketClientService {
     const tickets = await query.getMany();
 
     return tickets;
+  }
+
+  async chooseTechnician(): Promise<Technician> {
+    const technicians = await this.technicianRepository.find({});
+    if (technicians.length === 0) {
+      throw new NotFoundException(
+        `There is not technicians. Please try later.`,
+      );
+    }
+
+    let technician: Technician;
+    let i: number = 1;
+    while (true) {
+      technician = await this.technicianRepository.findOne({
+        status: TechnicianStatus.IDLE,
+      });
+      if (technician !== undefined) {
+        technician.status = TechnicianStatus.BUSY;
+        await technician.save();
+        break;
+      } else {
+        i++;
+        setTimeout(() => {
+          console.log('Please wait');
+        }, i * 3000);
+      }
+    }
+
+    return technician;
   }
 }
